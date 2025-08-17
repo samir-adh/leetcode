397a630ae1a81b3751a070007fde1f28f10b1541
Samir Abdallah <abdousamir8@gmail.com>
2025-08-17 14:00:15 +0100

    redid findDuplicate in 4mn

--- findDuplicate.py
+++ findDuplicate.py
@@ -1,14 +1,12 @@
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
         for x in nums:
-            y = abs(x)-1
+            y = abs(x) - 1
             if nums[y] < 0:
-                return y +1 
-            nums[y] *=-1
+                return abs(x)
+            else:
+                nums[y] *= -1
         return -1
-
-            
-        
 
 
 def test_case1():
@@ -19,18 +17,18 @@ def test_case1():
 
 
 def test_case2():
-    nums = [3,1,3,4,2]
+    nums = [3, 1, 3, 4, 2]
     expected = 3
     output = Solution().findDuplicate(nums)
     assert expected == output, f"expected {expected}, got {output}"
 
 
 def test_case3():
-    nums = [3,3,3,3,3]
+    nums = [3, 3, 3, 3, 3]
     expected = 3
     output = Solution().findDuplicate(nums)
     assert expected == output, f"expected {expected}, got {output}"
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     test_case2()
