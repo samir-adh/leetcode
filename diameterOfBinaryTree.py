5ad743c97a142d9b93a6dcaa3ec3230069b0469c
Samir Abdallah <abdousamir8@gmail.com>
2025-08-20 17:03:40 +0100

    redid diameter of binary tree in 5mn

--- diameterOfBinaryTree.py
+++ diameterOfBinaryTree.py
@@ -1,28 +1,19 @@
 # Definition for a binary tree node.
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        diameter = 0
-
-        def aux(node: Optional[TreeNode]):
-            nonlocal diameter
+        def height(node, diameter ):
             if node is None:
                 return 0
-
-            leftHeight = aux(node.left)
-            rightHeight = aux(node.right)
-
-            diameter = max(leftHeight + rightHeight, diameter)
-            return max(leftHeight, rightHeight) + 1
-
-        aux(root)
-        return diameter
+            else :
+                leftHeight = height(node.left, diameter)
+                rightHeight = height(node.right, diameter)
+                diameter[0] = max(diameter[0], leftHeight+rightHeight)
+                return max(leftHeight, rightHeight) + 1
+        diameter = [0]
+        height(root, diameter)
+        return diameter[0]
