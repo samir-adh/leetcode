2a5aa62de6225f225e873c8a6e364957c588527f
Samir Abdallah <abdousamir8@gmail.com>
2025-08-22 18:36:44 +0100

    redid tree is balanced in 3mn

--- trees/isBalanced.py
+++ trees/isBalanced.py
@@ -1,28 +1,22 @@
-from typing import Optional
-
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node, balanced) -> int:
-            if not balanced[0]:
+        balanced = [True]
+        def dfs(node, balanced):
+            if not balanced:
                 return -1
-            if node is None:
+            if node == None:
                 return 0
             else:
-                left = dfs(node.left, balanced)
-                right = dfs(node.right, balanced)
-                if abs(left - right) > 1:
+                l= dfs(node.left, balanced)
+                r = dfs(node.right, balanced)
+                if abs(l-r) > 1:
                     balanced[0] = False
-                return max(left, right) + 1
-
-        balanced = [True]
-        dfs(root, balanced)
+                return max(l,r) + 1
+        dfs(root,balanced)
         return balanced[0]
