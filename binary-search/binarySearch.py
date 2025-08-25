0891b0a585e152288451c309a0f4002b37720470
Samir Abdallah <abdousamir8@gmail.com>
2025-08-25 18:30:06 +0100

    redid search 2d matrix in 17mn : too long

--- binary-search/binarySearch.py
+++ binary-search/binarySearch.py
@@ -1,39 +1,25 @@
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
         left = 0
-        right = len(nums)-1
-        while  left <= right:
-            middle = left + (right - left) // 2
-            a = nums[middle]
-            if a == target:
-                return middle
-            elif a < target:
-                left = middle + 1
+        right = n*m
+
+        def toidx(k):
+            i = k // n
+            j = k % n 
+            return i,j
+        
+        while left <= right:
+            k = left + (right-left)//2
+            i,j = toidx(k)
+            if i >= m:
+                return False
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                left = k+1
             else :
-                right = middle -1
-        return -1 
-    
-
-def test_case1():
-    input = [-1,0,3,5,9,12]
-    target = 9
-    expected = 4
-    output = Solution().search(input, target)
-    assert expected == output
-
-def test_case2():
-    input = [-1,0,3,5,9,12]
-    target = 2
-    expected = -1
-    output = Solution().search(input, target)
-    assert expected == output
-
-def test_case3():
-    input = [5]
-    target = 5
-    expected = 0
-    output = Solution().search(input, target)
-    assert expected == output
-
-if __name__ =="__main__":
-    test_case3()
+                right = k-1
+        return False
