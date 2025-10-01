309d01185ab88e24d129b58780821530f0041d86
Samir Abdallah <abdousamir8@gmail.com>
2025-10-01 15:13:04 +0200

    redid pacific atlantic in 30mn - to do again

--- graphs/pacificAtlantic.py
+++ graphs/pacificAtlantic.py
@@ -6,53 +6,32 @@ class Solution:
         n = len(heights)
         m = len(heights[0])
 
-        def pac(i, j):
-            return i < 0 or (i < n and j < 0)
+        atl = set()
+        pac = set()
 
-        def atl(i, j):
-            return (i >= n or j >= n) and not pac(i, j)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        output = set()
-        cache = {}
-
-        def inGrid(i, j):
-            return 0 <= i < n and 0 <= j < m
-
-        def dfs(i: int, j: int, path: set):
-            if (i, j) in cache:
-                return cache[(i, j)]
-            if not inGrid(i, j):
-                p, a = pac(i, j), atl(i, j)
-                # print(f"p:{p} a :{a}")
-                if p and a:
-                    output.add((i, j))
-                cache[(i, j)] = (p, a)
-                return p, a
-            else:
-                p, a = False, False
-                for di, dj in directions:
-                    u = i + di
-                    v = j + dj
-                    if (u, v) not in path:
-                        if inGrid(u, v) and heights[u][v] > heights[i][j]:
-                            continue
-                        path.add((u, v))
-                        dp, da = dfs(u, v, path)
-                        path.remove((u, v))
-                        p = p or dp
-                        a = a or da
-                if p and a:
-                    # print(f"p:{p} a :{a}")
-                    output.add((i, j))
-                cache[(i, j)] = (p, a)
-                return p, a
+        def dfs(i: int, j: int, path: set[tuple[int, int]]):
+            if (i, j) in path:  # (0 <= i < n and 0<= j < m):
+                return
+            path.add((i, j))
+            for di, dj in directions:
+                u, v = i + di, j + dj
+                if (0 <= u < n and 0 <= v < m) and heights[i][j] <= heights[u][v]:
+                    dfs(u, v, path)
 
         for i in range(n):
+            dfs(i, 0, pac)
+            dfs(i, m - 1, atl)
+        for j in range(m):
+            dfs(0, j, pac)
+            dfs(n - 1, j, atl)
+        output = []
+        for i in range(n):
             for j in range(m):
-                dfs(i, j, set())
-
-        return [[i, j] for i, j in output]
+                if (i, j) in atl and (i, j) in pac:
+                    output.append([i, j])
+        return output
 
 
 def case1():
@@ -77,4 +56,4 @@ def case2():
     print(f"expected {expected}\ngot {output}")
 
 
-case2()
+case1()
