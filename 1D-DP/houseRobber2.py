34ff65ea5c15531e948ba5fc90efa7b02072e893
Samir Abdallah <abdousamir8@gmail.com>
2025-10-08 23:01:58 +0200

    redid house robber 2 in 10mn

--- 1D-DP/houseRobber2.py
+++ 1D-DP/houseRobber2.py
@@ -1,24 +1,26 @@
 from typing import List
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob = [0]*2
-        nums1 = nums[:len(nums)-1]
-        for n in nums1:
-            tmp = max(rob[0] + n, rob[1])
-            rob[0] = rob[1]
-            rob[1] = tmp
-        rob1 =  rob[1]
-        nums2 = nums[1:]
-        rob = [0]*2
-        for n in nums2:
-            tmp = max(rob[0] + n, rob[1])
-            rob[0] = rob[1]
-            rob[1] = tmp
-        rob2 =  rob[1]
-        return max(rob1, rob2)
-nums = [3,4,3]
+        if len(nums) < 3:
+            return max(nums)
+
+        def aux(nums: List[int]):
+            if len(nums) < 3:
+                return max(nums)
+
+            res = nums[:3]
+            res[1] = max(res[:2])
+            for i in range(2, len(nums)):
+                res[2] = max(res[1], res[0] + nums[i])
+                res[:2] = res[1:]
+            return res[2]
+
+        return max(aux(nums[1:]), aux(nums[: len(nums) - 1]))
+
+
+nums = [3, 4, 3]
 expected = 4
 output = Solution().rob(nums)
 print(f"expected {expected}, got {output}")
-
-
