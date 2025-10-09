e22e580682e713a6039dfb104b6333133ec28b19
Samir Abdallah <abdousamir8@gmail.com>
2025-10-09 14:46:00 +0200

    solved longestPalindrome in 1h

--- 1D-DP/longestPalindrome.py
+++ 1D-DP/longestPalindrome.py
@@ -1,14 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        substrings = []
+        maxpali = ""
         for i in range(n):
-            for j in range(i, n):
-                substrings.append(s[i : j + 1])
-        return substrings
+            # Case odd
+            left = i
+            right = i
+            res = s[left]
+            while left >= 0 and right < n and s[left] == s[right]:
+                res = s[left : right + 1]
+                left -= 1
+                right += 1
+            if len(res) > len(maxpali):
+                maxpali = res
+            # Case even
+            left = i
+            right = i + 1
+            res = ""
+            while left >= 0 and right < n and s[left] == s[right]:
+                res = s[left : right + 1]
+                left -= 1
+                right += 1
+            if len(res) > len(maxpali):
+                maxpali = res
+
+        return maxpali
 
 
-s = "babad"
+s = "aacabdkacaa"
 expected = "bab"
 output = Solution().longestPalindrome(s)
 print(f"expected {expected} got {output}")
