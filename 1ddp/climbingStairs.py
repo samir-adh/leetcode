13f3019bdaf8e9d3da65b976a5dcdb0212485f75
Samir Abdallah <abdousamir8@gmail.com>
2025-10-19 18:24:28 +0200

    redid climbing stairs in 5mn

--- 1ddp/climbingStairs.py
+++ 1ddp/climbingStairs.py
@@ -1,11 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        res = [0] * 3
-        res[0] = 1
-        res[1] = 2
+        res = [1, 2, 0]
         for i in range(2, n):
             res[i % 3] = res[(i - 1) % 3] + res[(i - 2) % 3]
-        return res[2]
+        return res[(n - 1) % 3]
 
 
 if __name__ == "__main__":
