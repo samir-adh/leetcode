907cfbf3bbdfd3f4af5c9ac0988a3a14ba62f15b
Samir Abdallah <abdousamir8@gmail.com>
2025-10-19 18:19:02 +0200

    solved build tree in 20mn

--- trees/buildTree.py
+++ trees/buildTree.py
@@ -1,24 +1,29 @@
+from typing import List, Optional
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        intable = {v:k for k,v in enumerate(inorder)}
-        self.preIdx = 0
-        left = 0
-        right = len(preorder)
+        intable = {k: v for v, k in enumerate(inorder)}
+        preIdx = 0
 
-        def aux(l,r):
-            if l>= r:
+        def aux(left: int, right: int):
+            nonlocal preIdx
+            if left >= right:
                 return None
-            nodeVal = preorder[self.preIdx]
-            node = TreeNode(nodeVal)
-            self.preIdx += 1
-            node.left = aux(l, intable[nodeVal])
-            node.right = aux(intable[nodeVal] + 1, r)
+            curr = preorder[preIdx]
+            node = TreeNode(val=curr)
+            k = intable[curr]
+            preIdx += 1
+            node.left = aux(left, k)
+            node.right = aux(k + 1, right)
             return node
-        
-        return aux(left,right)
+
+        return aux(0, len(preorder))
