b91bada1d1f5030dd2398cc5e814b681882110f7
Samir Abdallah <abdousamir8@gmail.com>
2025-10-19 18:44:11 +0200

    redid house robber 2 in 7mn

--- 1ddp/houseRobber2.py
+++ 1ddp/houseRobber2.py
@@ -3,19 +3,17 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 3:
-            return max(nums)
+        if len(nums) == 1:
+            return nums[0]
 
         def aux(nums: List[int]):
-            if len(nums) < 3:
-                return max(nums)
-
-            res = nums[:3]
-            res[1] = max(res[:2])
-            for i in range(2, len(nums)):
-                res[2] = max(res[1], res[0] + nums[i])
-                res[:2] = res[1:]
-            return res[2]
+            nums_copy = [0] + nums
+            res = [0] * 3
+            n = len(nums_copy)
+            res[:2] = nums_copy[:2]
+            for i in range(2, n):
+                res[i % 3] = max(res[(i - 1) % 3], res[(i - 2) % 3] + nums_copy[i])
+            return res[(n - 1) % 3]
 
         return max(aux(nums[1:]), aux(nums[: len(nums) - 1]))
 
