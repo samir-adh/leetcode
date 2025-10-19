a9b05e0e58cecd98069435e129cdff4474d12b3b
Samir Abdallah <abdousamir8@gmail.com>
2025-10-19 17:37:22 +0200

    redid kth smallest in 15mn

--- trees/kthSmallest.py
+++ trees/kthSmallest.py
@@ -1,21 +1,27 @@
+from typing import Optional
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        stack = []
         current = root
-        count = 0
-
+        stack = []
+        i = 1
         while stack or current:
-            while current :
+            while current:
                 stack.append(current)
                 current = current.left
-            current = stack.pop()
-            count += 1
-            if count == k:
+            current = stack.pop(-1)
+            if i == k:
                 return current.val
+            else:
+                i += 1
             current = current.right
+        return 0
