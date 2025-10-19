d4b6f9baa20308f51d23202715289477c65ad5df
Samir Abdallah <abdousamir8@gmail.com>
2025-10-19 18:29:23 +0200

    redid min cost climbing stairs in 5mn

--- 1ddp/minCostClimbingStairs.py
+++ 1ddp/minCostClimbingStairs.py
@@ -3,14 +3,13 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
-        n = len(cost)
         res = [0] * 3
+        cost.append(0)
         res[:2] = cost[:2]
+        n = len(cost)
         for i in range(2, n):
-            res[i % 3] = cost[i] + min(res[(i - 1) % 3], res[(i - 2) % 3])
-
-        return res[(len(cost) - 1) % 3]
+            res[i % 3] = min(res[(i - 1) % 3], res[(i - 2) % 3]) + cost[i]
+        return res[(n - 1) % 3]
 
 
 def test_case1():
