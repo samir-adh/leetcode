6501defa7000757eb2506d26edc5d73b99c8e5a6
Samir Abdallah <abdousamir8@gmail.com>
2025-10-20 14:49:21 +0200

    redid max product in 10mn

--- 1ddp/maxProduct.py
+++ 1ddp/maxProduct.py
@@ -3,28 +3,24 @@ from typing import List
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         n = len(nums)
-        pre = 1
+        result = -11
+        prev = 1
         post = 1
-        maxp = -11
-        for i in range(0,n):
-            if nums[i] ==  0:
-                pre = 1
-                maxp= max(0, maxp)
-            else :
-                pre *= nums[i]
-                maxp= max(pre, maxp)
-
-            if nums[n-i-1] ==  0:
+        for i in range(n):
+            if prev == 0:
+                prev = 1
+            p = nums[i] * prev
+            prev =p 
+            if post == 0:
                 post = 1
-                maxp= max(0, maxp)
-            else :
-                post *= nums[n-i-1]
-                maxp= max(post, maxp)
-            print(f'pre {pre}, post {post}')
-        return maxp
+            q = nums[n-1-i] * post
+            result = max(result,p, q)
+            post = q
+        return result
+
 
 def case1():
-    nums = [-2,0,-1]
+    nums = [2,3,-2,4]
     expected = 6
     output = Solution().maxProduct(nums)
     print(f"expected {expected}, got {output}")
