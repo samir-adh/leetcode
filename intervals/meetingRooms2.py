bd413600d36fd0bc1f581aae1be67b473f89ceed
Samir Abdallah <abdousamir8@gmail.com>
2025-11-20 16:47:41 +0100

    solved meeting rooms 2 with sweep line algorithm

--- intervals/meetingRooms2.py
+++ intervals/meetingRooms2.py
@@ -2,7 +2,6 @@
 Definition of Interval:
 """
 from typing import List
-from _heapq import heappush, heapreplace
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
@@ -11,13 +10,21 @@ class Interval(object):
 
 class Solution:
     def minMeetingRooms(self, intervals: List[Interval]) -> int:
-        if len(intervals) == 0:
-            return 0
-        intervals = sorted(intervals, key=lambda x : x.start)
-        days_ends = [0]
-        for meeting in intervals:
-            if meeting.start >= days_ends[0]:
-                heapreplace(days_ends,meeting.end)
-            else :
-                heappush(days_ends, meeting.end)
-        return len(days_ends)
+        timestamps = []
+        for interval in intervals:
+            timestamps.append((interval.start, 1))
+            timestamps.append((interval.end, -1))
+
+        timestamps = sorted(timestamps, key = lambda x : x[0])
+        current_number_of_rooms = 0
+        max_number_of_rooms = 0
+        for i, timestamp in enumerate(timestamps[:len(timestamps)-1]):
+            current_number_of_rooms += timestamp[1]
+            if timestamps[i+1][0] != timestamp[0]:
+                max_number_of_rooms = max(max_number_of_rooms, current_number_of_rooms)
+        return max_number_of_rooms
+    
+intervals=[(0,50),(10,60),(60,110),(70,120),(20,70),(30,80),(40,90),(50,100),(80,130),(90,140),(100,150)]
+intervals=[(1,5),(2,6),(3,7),(4,8),(5,9)]
+output = Solution().minMeetingRooms([Interval(x,y) for x,y in intervals])
+print(output)
