43c4a538c1ac8780903d27bcd64e6bc6775b45e5
Samir Abdallah <abdousamir8@gmail.com>
2025-11-21 18:57:32 +0100

    redid insert interval in 20mn

--- intervals/insertInterval.py
+++ intervals/insertInterval.py
@@ -1,25 +1,36 @@
 from typing import List
-
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        output = []
-        s,e = newInterval
-        for i, (i_s,i_e) in enumerate(intervals):
-            if e < i_s :
-            # we will never be able to insert it anymore so we do it now
-                output.append([s,e])
-                return output + intervals[i:]
-            if s > i_e:
-            # then we'll have to insert the interval after
-                output.append([i_s,i_e])
-            else:
-                # that means the start or the end of new interval is in the middle of the current one
-                # so we fuse them and try to insert the newly formed interval
-                s = min(s, i_s)
-                e= max(e, i_e)
-        output.append([s,e])
-        return output
-intervals=[[1,3],[6,9]]
-newInterval=[2,5]
-out = Solution().insert(intervals , newInterval)
-print(out)
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+        def findInsertion(intervals : List[List[int]], key: int, value: int):
+            left = 0
+            right = len(intervals)
+            mid = -1
+            prev = 0
+            while mid != prev:
+                prev = mid
+                mid = (left + right) // 2
+                if intervals[mid][key] == value:
+                    return mid
+                if intervals[mid][key] >= value:
+                    right = mid
+                else:
+                    left = mid
+            return right
+
+        intervals = sorted(intervals, key=lambda x: x[0])
+        s, e = newInterval
+        s_index = findInsertion(intervals, 0, s)
+        e_index = findInsertion(intervals, 1, e)
+        if s_index > 0 and intervals[s_index - 1][1] >= s:
+            s_index -= 1
+            s = intervals[s_index][0]
+
+        if e_index < len(intervals) and intervals[e_index][0] <= e:
+            e = intervals[e_index][1]
+            e_index += 1
+
+        return intervals[:s_index] + [[s, e]] + intervals[e_index:]
