8b38379a032b242677e8eb95c7adf107d77e0fb4
Samir Abdallah <abdousamir8@gmail.com>
2025-12-17 22:10:26 +0100

    solved median of 2 arrays in 1h15

--- binary-search/findMedianSortedArrays.py
+++ binary-search/findMedianSortedArrays.py
@@ -1,50 +1,93 @@
-# what is the median of two sorted arrays A and B ?
-# is is the value x such that |n in A+B, n <=  x| = |n in A+B, n >= x|
-# for a single sorted array, its value is:
-#       (array[len(array)//2] + array[len(array)+1//2] )/2
-# if the the size of A+B = s, let t = (s+1)//2
-# we are looking for u,v such that
-# | n in A+B, n <= u | = t and | n in A+B, n >= v | = t
-# the mean will then be (u+v)/2
-# to find u and v we can use binary search :
-# left = -10e6, right = 10e6
-# prev = -1, mid = 0
+# ex : [1,2,3], [1,2,4] -> [1,1,2,2,3,4] -> median is (2+2)/2 = 2
+
+# Finding the median of an array  means finding a value that splits it in two equally sized parts.
+# This means the problem we are trying to solve is to find which items should be located at the left of the median of the hypothetical merged array resulting from the two.
+# We already know how many :
+# max_take := l//2 +1 items with l = m+n
+# if l is odd than the median is the largest element chosen
+# if  l is even it is the mean of the 2 largest elements chosen
+# To find which items to take, we'll use binary search to find the number of items to take from the longest array :
+# there are a few cases to consider, for example if we take 1 element from 'a' (meaning we have to take 3 from  'b') in our example we have :
+# l_part = {'a':[1], 'b':[1,2,4]} and r_part {'a':[2,3],'b':[]}
+# we know that it's wrong because r_part['a'][0]<l_part['b'][-1] meaning we should take more elements from 'a'
+# conversly, if we take 3 elements from 'a':
+# l_part = {'a':[1,2,3], 'b':[1]} and r_part {'a':[],'b':[2,4]}
+# we have r_part['b'][0]<l_part['a'][-1] meaning we should take less elements from 'a'
+# set 'a' to be the longest array, 'b' the shortest
+# our search space will be (max_take-len(b)):max_take-1, max_take - take_from_a <= len(b) => take_from_a >= max_take - len(b)
+# v := max_take
 #
-# example : we need to have 4 values on each size
-# A=1,7,7,7,7,7 B=2,3 ->  1,2,3,7,7,7,7,7 -> [1,7,4], [4,7,5],[5,7,6]
-# A=1,4,4 ; B=2,3,4 -> 1,2,3,4,4,4
-# halfsize = 3
-#
+# max_take = (m+n)//2 + 1; n > m
+# max_take >= 2m//2 + 1
+# max_take >= m + 1
+# max_take > m
+
 from typing import List
+
+
+def findMedian(nums: List[int]):
+    n = len(nums)
+    if n % 1:
+        return nums[n//2]
+    else:
+        return ((nums[(n-1)//2] + nums[n//2]))/2
 
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n, m = len(nums1), len(nums2)
-        if n > m:
-            n, m = m, n
+        if len(nums2) > len(nums1):
             nums1, nums2 = nums2, nums1
-        half = (n + m) // 2
-        left = 0
-        right = half
-        take = (left + right) // 2
-        prev = 0
-        while prev != take:
-            if nums1[min(take, n) - 1] > nums2[half - min(take, n)]:
-                left = take
-            elif nums2[half - min(take, n) - 1] > nums1[min(take, n)]:
-                right = take
+
+        n = len(nums1)
+        m = len(nums2)
+
+        if m == 0:
+            return findMedian(nums1)
+        full_size = m + n
+        left_size = full_size//2 + 1
+        max_take = left_size
+        min_take = max_take - m  # min_take is always gt 1
+        take1 = 0
+        take2 = 0
+        while min_take <= max_take:
+            take1 = (max_take + min_take)//2
+            take2 = left_size - take1
+            # take less elements from nums1
+            if take2 < m and nums1[take1-1] > nums2[take2]:
+                max_take = take1 - 1
+            # take more from nums1:
+            elif take1 < n and nums2[take2-1] > nums1[take1]:
+                min_take = take1 + 1
             else:
                 break
-            prev = take
-            take = (left + right) // 2
-        print(take)
-        return (nums1[min(take, n) - 1], nums2[half - min(take, n) - 1])
+        print(f"take1 : {take1}")
+        print(f"take2 : {take2}")
+        print(f"left_size: {left_size}")
+        if full_size % 2:  # full_size is odd
+            if take2  == 0:
+                return nums1[take1-1]
+            elif take1 == 0:
+                return nums2[take2-1]
+            else :
+                return max(nums1[take1-1], nums2[take2-1])
+        else:
+            candidates = []
+            if take1 > 0:
+                candidates.append(nums1[take1-1]) 
+            if take2 > 0:
+                candidates.append(nums2[take2-1])
+            if take1-2 >= 0:
+                candidates.append(nums1[take1-2])
+            if take2-2 >= 0:
+                candidates.append(nums2[take2-2])
+            print(f"candidates : {candidates}")
+            two_largest = sorted(candidates)[-2:]
+            return sum(two_largest)/2
 
 
-nums1 = [1, 2]
-nums2 = [3]
-# 2,2,2,3,4,4,4,4,5,6
-output = Solution().findMedianSortedArrays(nums1, nums2)
-print(output)
-print(sorted(nums1 + nums2))
+# a, b = [-10,-9,-8], [1,2]
+a, b = [4], [1,2,3]
+output = Solution().findMedianSortedArrays(a, b)
+expected = findMedian(sorted(a+b))
+print(sorted(a+b))
+print(f"got {output} expected {expected}")
